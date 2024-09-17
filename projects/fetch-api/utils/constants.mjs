a318e1c34827eb5b872b7b2d52a8c102d4878e2b
Jose <jmagarciadiez@gmail.com>
2024-09-17 08:55:56 -0300

    Create constants for styling upon type of pokemons

---
+++ projects/fetch-api/utils/constants.mjs
@@ -0,0 +1,54 @@
+export const POKEMON_TYPE_COLOR = {
+  bug: "#94bc4a",
+  dark: "#736c75",
+  dragon: "#6a7baf",
+  electric: "#e5c531",
+  fairy: "#e397d1",
+  fighting: "#cb5f48",
+  fire: "#ea7a3c",
+  flying: "#7da6de",
+  ghost: "#846ab6",
+  grass: "#71c558",
+  ground: "#cc9f4f",
+  ice: "#70cbd4",
+  normal: "#aab09f",
+  poison: "#b468b7",
+  psychic: "#e5709b",
+  rock: "#b2a061",
+  steel: "#89a1b0",
+  water: "#539ae2",
+};
+
+export const POKEMON_TYPE_GRADIENT = {
+  bug: "linear-gradient(180deg, hsla(81, 46%, 51%, 1) 31%, hsla(0, 0%, 100%, 1) 86%)",
+  dark: "linear-gradient(180deg, hsla(287, 4%, 44%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  dragon:
+    "linear-gradient(180deg, hsla(225, 30%, 55%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  electric:
+    "linear-gradient(180deg, hsla(49, 78%, 55%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  fairy:
+    "linear-gradient(180deg, hsla(314, 58%, 74%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  fighting:
+    "linear-gradient(180deg, hsla(11, 56%, 54%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  fire: "linear-gradient(180deg, hsla(21, 81%, 58%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  flying:
+    "linear-gradient(180deg, hsla(215, 60%, 68%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  ghost:
+    "linear-gradient(180deg, hsla(261, 34%, 56%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  grass:
+    "linear-gradient(180deg, hsla(106, 48%, 56%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  ground:
+    "linear-gradient(180deg, hsla(38, 55%, 55%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  ice: "linear-gradient(180deg, hsla(185, 54%, 64%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  normal:
+    "linear-gradient(180deg, hsla(81, 10%, 66%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  poison:
+    "linear-gradient(180deg, hsla(298, 35%, 56%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  psychic:
+    "linear-gradient(180deg, hsla(338, 69%, 67%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  rock: "linear-gradient(180deg, hsla(47, 34%, 54%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  steel:
+    "linear-gradient(180deg, hsla(203, 20%, 61%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+  water:
+    "linear-gradient(180deg, hsla(210, 71%, 61%, 1) 30%, hsla(0, 0%, 100%, 1) 86%)",
+};
